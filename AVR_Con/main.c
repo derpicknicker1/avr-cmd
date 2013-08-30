42b0fe269a80caac75393a57f5a2318804d7c1ba
bademeistor@googlemail.com <bademeistor@googlemail.com@d314fce8-9c7f-7c37-fc9f-ffffa6d8ef19>
2013-08-30 04:10:45 +0000

    - SD-Card support added

--- AVR_Con/main.c
+++ AVR_Con/main.c
@@ -4,10 +4,19 @@
  *  Created on: 28.08.2013
  *      Author: Grisu
  */
-#include <util/delay.h>
+
+#include <avr/interrupt.h>
 #include "usart.h"
 #include "cmd.h"
-#include <avr/interrupt.h>
+
+#define USE_SD 1 //also set in cmd.h
+
+#if USE_SD == 1
+	#include "sd/mmc_config.h"	// Hier werden alle noetigen Konfigurationen vorgenommen, umbedingt anschauen !
+	#include "sd/file.h"
+	#include "sd/fat.h"
+	#include "sd/mmc.h"
+#endif
 
 
 int main(void){
@@ -21,6 +30,28 @@ int main(void){
 	PORTD=0x00;
 	usart_init(9600);
 	sei();
+	usart_write_str(CRLL"AVR-Con alpha 0.1"CRLF);
+#if USE_SD == 1
+	usart_write_str("Boot SD");
+
+	// sd/mmc config  **************************************************
+	if( FALSE == mmc_init() ){
+		usart_write_str(CRLF"ERR | MMC-Init: System halted");
+		return 0;
+	}
+
+	usart_write_str("...");
+
+	// fat config ******************************************************
+	if( FALSE == fat_loadFatData() ){
+		usart_write_str(CRLF"ERR | FAT-Init: System halted");
+		return 0;
+	}
+
+	// wenn auf dem terminal "Boot...OK" zu lesen ist, war initialisierung erfolgreich!
+	usart_write_str("OK"CRLL);
+#endif
+
 
 	while(1){
 
@@ -28,18 +59,8 @@ int main(void){
 		usart_status.usart_ready=0;
 
 		while(!usart_status.usart_ready);
-				if(stringLength(usart_rx_buffer) < 2){
-					//printf("Makro: %s\n",line);
-					if(stringCompare(usart_rx_buffer,"W") ==0){
-						parseLine("set power 1");
-					}
-					if(stringCompare(usart_rx_buffer,"w") == 0){
-						parseLine("set power 0");
-					}
-				}
-				else{
-					parseLine(usart_rx_buffer);
-				}
+		usart_write_str(CRLF);
+		parseLine(usart_rx_buffer);
 
 	}
 

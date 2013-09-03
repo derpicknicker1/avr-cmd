6b829332dcb3f7e9b117baa249ff2a07ed56904f
bademeistor@googlemail.com <bademeistor@googlemail.com@d314fce8-9c7f-7c37-fc9f-ffffa6d8ef19>
2013-09-03 19:41:03 +0000

    Initial Commit for Mega2560 Version

---
+++ trunk/AVR_CMD/cmd.h
@@ -0,0 +1,43 @@
+/*
+ * cmd.h
+ *
+ *  Created on: 28.08.2013
+ *      Author: Grisu
+ */
+
+#ifndef CMD_H_
+	#define CMD_H_
+
+//	#define ADD(x,y)		(x+=y)
+//	#define SUB(x,y)		(x-=y)
+//	#define MUL(x,y)		(x*=y)
+//	#define MOD(x,y)		(x%=y)
+//	#define DIV(x,y)		(x/=y)
+
+	#define PIN_REG (0x00)
+	#define DDR_REG (0x01)
+	#define PORT_REG (0x02)
+
+enum{
+	ERROR 	= -1,	//	-1 = error
+	CONV_D 	=  0,	//	 0 = decimal
+	CONV_H 	=  1,	//	 1 = hex
+	CONV_B 	=  2,	//	 2 = binary
+	CONV_O 	=  3,	// 	 3 = octal
+	CONV_SW =  4,	//	 4 = on/off/in/out
+	CONV_REG=  5,	//	 5 = Register
+	CONV_VA =  6,	//	 6 = Var
+	CONV_ARG=  7	// 	 7 = File-Arg
+};
+
+	typedef struct
+	{
+		char* cmd; 			//CMD Name
+		int8_t(*fp)(void);  	// function pointer
+	} COMMAND_STRUCTUR;
+
+	void parse_line(char* line);
+	void file_args_init(void);
+	void uint_to_bin(char* out, uint16_t value);
+
+#endif /* CMD_H_ */
---
+++ trunk/AVR_CMD/main.c
@@ -0,0 +1,98 @@
+/*
+ * main.c
+ *
+ *  Created on: 28.08.2013
+ *      Author: Grisu
+ *
+ *      This is the Version for Mega2560 Hardware.
+ *      Using USART0 @ 9600Baud,8N1
+ */
+
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "config.h"
+#include "usart.h"
+#include "cmd.h"
+#include "string.h"
+
+#if USE_SD == 1
+	#include "sd/mmc_config.h"
+	#include "sd/file.h"
+	#include "sd/fat.h"
+	#include "sd/mmc.h"
+#endif
+
+FILE mystdout = FDEV_SETUP_STREAM(uart_putc, NULL, _FDEV_SETUP_WRITE);
+
+int main(void){
+	DDRA=0xFF;
+	DDRB=0xFF;
+	DDRC=0xFF;
+	DDRD=0xFF;
+	PORTA=0x00;
+	PORTB=0x00;
+	PORTC=0x00;
+	PORTD=0x00;
+	uart_init(9600);
+	stdout = &mystdout;
+	sei();
+
+	printf(ESC_CLS""ESC_GREEN""ESC_BOLD"AVR-Con alpha 0.1"CRLF""ESC_CLEAR);
+
+	file_args_init();
+#if USE_SD == 1
+	printf("Boot SD");
+
+	if( FALSE == mmc_init() ){
+		printf(CRLF""ESC_RED"ERR | MMC-Init: System halted"ESC_CLEAR);
+		return 0;
+	}
+
+	printf("...");
+
+	if( FALSE == fat_loadFatData() ){
+		printf(CRLF""ESC_RED"ERR | FAT-Init: System halted"ESC_CLEAR);
+		return 0;
+	}
+#endif
+
+	printf(ESC_GREEN"OK"ESC_CLEAR""CRLL"> ");
+	while(1){
+
+		if(usart_status.usart_ready==1){
+			printf(CRLF);
+
+			//parse line (exec cmd)
+			parse_line(usart_rx_buffer);
+
+			//add line to cmd history
+			hist_add(strcpy(malloc((strlen(usart_rx_buffer) + 1)*sizeof(char)),usart_rx_buffer));
+
+			//get ready for next line
+			printf(CRLF"> ");
+			usart_status.usart_ready=0;
+
+		}
+
+
+
+
+
+
+	}
+	
+	/*        .==.        .==.
+	//       //`^\\      //^`\\
+	//      // ^ ^\(\__/)/^ ^^\\
+	//     //^ ^^ ^/6  6\ ^^ ^ \\
+	//    //^ ^^ ^/( .. )\^ ^ ^ \\
+	//   // ^^ ^/\| v""v |/\^ ^ ^\\
+	//  // ^^/\/ /  `~~`  \ \/\^ ^\\
+	//  -----------------------------
+	/// HERE BE DRAGONS */
+
+	return 0; //returns 0
+	
+}
+
